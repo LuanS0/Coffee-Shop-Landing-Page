a8935bc1c2484bf0618d121e5a66b7dcfd75dfb1
luans0 <luan.souza07@outlook.com>
2023-04-09 23:07:39 -0300

    This is a Coffee Shop Landing Page :coffee:

--- assets/js/swiper/app-swiper.js
+++ assets/js/swiper/app-swiper.js
@@ -1,17 +1,3 @@
-const swiper = new Swiper('.swiper',{
-    grabCursor: true,
-    effect: 'creative',
-    autoplay: {
-      delay: 5000,
-    },
-    creativeEffect: {
-        prev: {
-          shadow: true,
-          translate: [0, 0, -400],
-        },
-        next: {
-          translate: ["100%", 0, 0],
-        },
-      },
-      loop: true,
+const swiper = new Swiper('.swiper', {
+    effect: 'coverflow',
 });
