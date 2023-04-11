387dc74f3c67c98f30d26496bd107f20338737b5
LuanS0 <luan.azevedo.souza.profissional@gmail.com>
2023-04-10 23:24:03 -0300

    Thid is a LP new version :coffee:

--- assets/js/swiper/app-swiper.js
+++ assets/js/swiper/app-swiper.js
@@ -1,3 +1,25 @@
-const swiper = new Swiper('.swiper', {
+var mySwiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+    },
     effect: 'coverflow',
-});
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 10,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+})
